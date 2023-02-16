e5dc01ebd351f620b5a86f7f684732bccb946044
GreenFox77 <oitbibn@gmail.com>
2023-02-16 13:47:18 -0800

    added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,18 @@
-#include <unistd.h>
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a quote using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: This time we return an error 1.
+*/
 
 int main(void)
 {
-    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(str) - 1;
-    ssize_t bytes_written = write(STDERR_FILENO, str, len);
-    return (bytes_written == len) ? 1 : -1;
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
 }
