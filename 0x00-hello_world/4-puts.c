4d6902ab0d26df6174020d45854894493ee9887b
GreenFox77 <oitbibn@gmail.com>
2023-02-16 11:22:33 -0800

    added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Discreption: A C program that prints with puts function
+ *
+ * Return: alwys 0 (success)
+ */
+
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
