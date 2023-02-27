a57329a492030207d5ac5d4830ac80a8776d25d3
GreenFox77 <oitbibn@gmail.com>
2023-02-27 14:23:52 -0800

    added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - finds length of a  string
+ *
+ * @s: pointer to the string
+ *
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (c >= 0);
+	{
+		if (s[c] == '\0')
+		{
+			break;
+		}
+		else
+		{
+			C++;
+		}
+	}
+	return (c);
+	return (0);
+}
